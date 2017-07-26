2e15579553f701a9b7d5bb24fc3e742e208e2901
MarkWojciechowicz <MWojciechowicz@anexinet.com>
2017-07-26 07:35:01 -0400

    Added project image Added tests, removed strongly signed assembly for now in order to test internal methods and allow for easier collaboration.  This can be sigend before uploading to nuget

--- SqlTest.Tests/SqlTestTarget.cs
+++ SqlTest.Tests/SqlTestTarget.cs
@@ -5,19 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using System.Transactions;
+using Microsoft.SqlServer.Management.Smo;
 
 namespace SqlTest.Tests
 {
     public class SqlTestTarget
     {
-        TransactionScope scope;
-        SqlTest.SqlTestTarget testTarget;
+        private TransactionScope _scope;
+        private SqlTest.SqlTestTarget _testTarget;
 
         [SetUp]
         public void Setup()
         {
-            scope = new TransactionScope();
-            testTarget = new SqlTest.SqlTestTarget("testTarget");
+            _scope = new TransactionScope();
+            _testTarget = new SqlTest.SqlTestTarget("testTarget");
         }
 
         [TearDown]
@@ -25,21 +26,21 @@ namespace SqlTest.Tests
         {
             if (Transaction.Current.TransactionInformation.Status == TransactionStatus.Active)
             {
-                scope.Dispose();
+                _scope.Dispose();
             }
         }
 
         [Test]
         public void SqlTestTarget_ExecuteSetup_NoExceptionThrown()
         {
-            testTarget.ExecuteAdhoc($"Select 1;");
+            _testTarget.ExecuteAdhoc($"Select 1;");
         }
 
         [Test]
         public void SqlTestTarget_ExecuteSetup_ExceptionThrown()
         {
             Assert.Throws(typeof(Exception),
-                delegate { testTarget.ExecuteAdhoc($"Select 1/0;"); }
+                delegate { _testTarget.ExecuteAdhoc($"Select 1/0;"); }
             );
         }
 
@@ -47,8 +48,21 @@ namespace SqlTest.Tests
         [TestCase("Select '1' as MyString", "1")]
         public void SqlTestTarget_ExecuteGetValue_ReturnsValue(string sql, object expected)
         {
-            var actual = testTarget.GetActual(sql);
+            var actual = _testTarget.GetActual(sql);
             Assert.That(actual, Is.EqualTo(expected));
         }
+
+        [Test]
+        public void SqlTestTarget_GetServerFromOledbConnString_ReturnsServer()
+        {
+            SqlTest.SqlTestTarget testTarget = new SqlTest.SqlTestTarget("testTarget");
+            var actual = testTarget.GetServer();
+            Assert.That(actual, Is.Not.Null);
+        }
+
+        //[Test]
+        //public void SqlTestTarget_InitialInstance_
+
+        
     }
 }
